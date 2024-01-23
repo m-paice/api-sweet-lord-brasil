0cbdd555f24bb3db2d51e3c4b339950d1aa39583
Matheus Paice <matheus.paice@jbtec.com.br>
2024-01-23 10:40:13 -0300

    fix: correct eslint config

--- src/utils/routing.ts
+++ src/utils/routing.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { ServerResponse } from 'http';
 
 type HandlerFunction = (req: Request, res: Response, next?: NextFunction) => Promise<any> | ServerResponse | any;
